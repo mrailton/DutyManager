96960afaed9ff6c2897eccbe4e4da93e84b9e2a7
Mark Railton <mark.railton@outlook.ie>
2026-07-14 23:30:18 +0100

    invokable controllers

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,9 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\MembersController;
+use App\Http\Controllers\Members\ListMembersController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', DashboardController::class)->name('dashboard');
 
 Route::prefix('members')->as('members.')->group(function (): void {
-    Route::get('/', [MembersController::class, 'index'])->name('index');
+    Route::get('/', ListMembersController::class)->name('index');
 });
